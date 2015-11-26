d2497bd6b4e7592e495e391eb48dbd29b83d4fcf
Felix <felix.friedberger@gmail.com>
2015-11-26 11:39:20 +0800

    Fixed HTML

--- pages.py
+++ pages.py
@@ -2,14 +2,16 @@
 def PrintError(Adress,Content):
 	ErrorImage = "error.jpg";
 	ErrorBackground = "#f3f4f4";
-	return '<body style="margin:0px;padding:0px;background-color:' + ErrorBackground + ';"><img src="' + Adress + ErrorImage + '" style="height:600px;display: inline-block;vertical-align: middle;margin-left: auto;margin-right: auto"></src>';
+	return '<body style="margin:0px;padding:0px;background-color:' + ErrorBackground + ';"><img src="' + Adress + ErrorImage + '" style=" position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%);"></src>';
 
 def PrintFullframeImage(Adress,Content):
-	return '<body style="margin:0px;padding:0px;background-image:url(\'' + Adress + 'img/' + content + '\');background-position: right top;background-size: 100%;">'
+	return '<body style="margin:0px;padding:0px;background-image:url(\'' + Adress + 'img/' + Content + '\');background-position: right top;background-size: 100%;">'
 
 def PrintCostumHTML(Adress,Content):
 	return '<body style = "margin:0px;padding:0px;">\n%s\n' % (Content)
 
+def PrintCenteredImage(Adress,Content):
+	return '<body style="margin:0px;padding:0px;"><img src="' + Adress + 'img/' + Content + '" style=" position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%);"></src>';
 
 #DO NOT EDIT
 #Backbone functions for server call and input
@@ -28,4 +30,4 @@ def GetPage(Adress,Type,Content,overlay):
 
 #Global Function Register
 global functions
-functions = {'0':PrintError,'3':PrintCostumHTML,'4':PrintFullframeImage};
+functions = {'0':PrintError,'3':PrintCostumHTML,'4':PrintFullframeImage,'5':PrintCenteredImage};

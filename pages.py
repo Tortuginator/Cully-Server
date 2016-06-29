e274b6ee7cca536d21641d381ec15f3ddad563c1
Felix <felix.friedberger@gmail.com>
2016-06-29 14:16:55 +0800

    Implemented renderer for room calendar

--- pages.py
+++ pages.py
@@ -174,6 +174,9 @@ def PrintSlideshowImage(Address, Content, ID):
 def PrintFullIFrame(Address,Content,ID):
 	return '<body style = "font-family:' + font +'!important;margin:0px;padding:0px;">\n<iframe src="' + Content + '" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999998;">Your browser doesn\'t support iframes</iframe>';
 
+def PrintRoomCalendar(Address,Content,ID):
+	return '<body style = "font-family:Open Sans!important;margin:0px;padding:0px;background-color:white;"><!--CALENDARINIT--><div id="roomname"><p id="text">' + Content.split("|")[0] + '</p></div><div id="roomcal"></div><lnk id="b64ical" style="display:none;">' + Content.split("|")[1] + '</lnk>';
+
 def PrintRSS(Address,Content,ID):
 	return "";
 
@@ -181,16 +184,16 @@ def PrintRSS(Address,Content,ID):
 
 #DO NOT EDIT
 #Backbone functions for server call and input
-def GetBackbone(innerHTML, debug,ticker = True):
+def GetBackbone(innerHTML, debug,ticker = True,type = -1):
 	if (innerHTML == None): innerHTML = "";
-	typefaceCSS = '<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,300" rel="stylesheet" type="text/css"><link rel="stylesheet" type="text/css" href="../assets/breakingNews.css">';
+	typefaceCSS = '<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,300" rel="stylesheet" type="text/css"><link rel="stylesheet" type="text/css" href="../assets/breakingNews.css"><link rel="stylesheet" href="../assets/fullcalendar.min.css" /><link rel="stylesheet" href="../assets/calendar.css" />';
 	htmlCSS = "font-family: '" + font + "', sans-serif !important;"
 	if debug == True:
 		debugHTML = '<div style="background-color:black;color:white;position:fixed;top:50px;left:50px;padding:5px;text-align:center;font-size:25;">Developermode <p style="display:inline;color:green;">Active</p> | Connection <p style="display:inline;color:green;">Active</p></div>\n<div id="debug_lower" style="background-color:black;color:white;position:fixed;bottom:50px;right:50px;padding:5px;text-align:center;font-size:25;"></div>\n<div id="debug_upper" style="background-color:black;color:white;position:fixed;top:50px;right:50px;padding:5px;text-align:center;font-size:25;"></div>\n<div id="special_frame" style=""></div>';
 	else:
 		debugHTML = '<div id="debug_lower" style="display:none;"></div>\n<div id="debug_upper" style="display:none;"></div>\n<div id="special_frame" style=""></div>';
-	if ticker is True:
-		ticker = """<div id="bbcticker" style='width: 100%;position:fixed;bottom:0px;left:0px;'><div id="bn1" style="padding:0; margin:0; font-family:Open Sans; font-size:30px;" class="breakingNews bn-bordernone bn-red"><div class="bn-title" style="width: auto;"><h2 style="display: inline-block;">BBC News</h2><span></span></div><ul style=""><li style="display: block; width: 100%;"><a href="" target="_blank">""" + GetTicker() + """</a></li></ul></div></div>""";
+	if ticker is True and type != 11:
+		ticker = """<div id="bbcticker" style='z-index:9999;width: 100%;position:fixed;bottom:0px;left:0px;'><div id="bn1" style="padding:0; margin:0; font-family:Open Sans; font-size:30px;" class="breakingNews bn-bordernone bn-red"><div class="bn-title" style="width: auto;"><h2 style="display: inline-block;">BBC News</h2><span></span></div><ul style=""><li style="display: block; width: 100%;"><a href="" target="_blank">""" + GetTicker() + """</a></li></ul></div></div>""";
 	else:
 		ticker = "";
 	return typefaceCSS + "\n" + "<html style=\"" + htmlCSS + "\">\n" + innerHTML + "\n</body>\n" + ticker + "\n" + debugHTML +  "\n</html>";
@@ -201,21 +204,22 @@ def GetPage(Type, Content, ID, Configuration):
 		global lConfig
 		lConfig = Configuration
 
-		functions = {'0': PrintError, '2': PrintCostumHTML, '3': PrintCostumHTML, '4': PrintFullframeImage, '5': PrintCenteredImage, '6': PrintSlideshowImage, '7': PrintCalendar, '8': PrintFullIFrame, '9': PrintYoutube,'10':PrintRSS};#add your customized functions here 'ID',FuntionName || '0' is predefined error
+		functions = {'0': PrintError, '2': PrintCostumHTML, '3': PrintCostumHTML, '4': PrintFullframeImage, '5': PrintCenteredImage, '6': PrintSlideshowImage, '7': PrintCalendar, '8': PrintFullIFrame, '9': PrintYoutube,'10':PrintRSS,'11':PrintRoomCalendar};#add your customized functions here 'ID',FuntionName || '0' is predefined error
 		Type = int(Type);
 		if not ID in ItemStorage:
 			ItemStorage[ID] = dict();
 
 		if str(Type) in functions:
-			return GetBackbone(functions[str(Type)](Configuration["ADDR"], Content, ID),debug);
+			return GetBackbone(functions[str(Type)](Configuration["ADDR"], Content, ID),debug, type = int(Type));
 		else:
-			return GetBackbone(functions['0'](Configuration["ADDR"], Content, ID), debug)
+			return GetBackbone(functions['0'](Configuration["ADDR"], Content, ID), debug, type = int(Type))
 	except Exception,e:
 		print "[!][CRITICAL] Unexpected error:", sys.exc_info()
 		logging.exception("", exc_info=True)
 
 def GetTicker():
 	url = "http://feeds.bbci.co.uk/news/rss.xml?edition=int";
+	limit = 15;#max number of newsitems displayed in one cycle
 	global ticker_timer
 	global ticker_current
 	global ticker_storage
